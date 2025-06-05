6f5ea4dea890c0d9a54bc524e4508106aa73fef7
Abhinav Srivatsa Kondapalli <121254293+SerDunk@users.noreply.github.com>
2025-06-05 12:13:52 +0530

    Scrollbar and spacing fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,10 @@ export default function RootLayout({ children }) {
     <html lang="en" className={lexend.variable}>
       <body className="bg-[#EFEDF4] min-h-screen flex flex-col">
         <Navbar />
-        <main className="container mx-auto px-4 py-8 flex-grow">
+        <main className="container mx-auto px-4 py-4 flex-grow">
           {children}
         </main>
-        <footer className="bg-gradient-to-r from-[#583FCA] to-[#2D14A0] text-white py-6">
+        <footer className="bg-gradient-to-r from-[#583FCA] to-[#2D14A0] text-white py-2 mt-4 sm:py-4">
           <div className="container mx-auto px-4 text-center">
             <p>© {new Date().getFullYear()} GoalSeek. All rights reserved.</p>
           </div>
